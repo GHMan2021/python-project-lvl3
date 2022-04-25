f2057a05b004b0d6fb1b40c43f580ea35cd9d97e
GHMan2021 <statio2009@gmail.com>
2022-04-25 23:27:50 +0400

    stage 3

--- page_loader/download.py
+++ page_loader/download.py
@@ -5,60 +5,93 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 
 
-def save_data(url, output_dir):
-    pattern = r'^https?\:\/\/|\.html\/?$|\/$'
-    name_url = re.sub(pattern, "", url)
-    # name_url = ru.hexlet.io/courses
+def check_tag(tag, u_netlog):
+    if any([tag.has_attr('src'), tag.has_attr('href')]):
+        attr = tag.get('href', tag.get('src'))
+        a = urlparse(attr)
+        if all([a.scheme != '', a.netloc != u_netlog]):
+            return False
+        return True
+    return False
 
-    pattern = r'[\.\:\/]'
-    name_html = re.sub(pattern, "-", name_url) + '.html'
+
+def conv_name(attr):
+    a = urlparse(attr)
+    pattern = r'[\.\:]'
+    a_netloc = re.sub(pattern, "-", a.netloc)
+    pattern = r'[\/]'
+    a_path = re.sub(pattern, "-", a.path)
+    if a_path.find('.') == -1:
+        a_path = a_path + '.html'
+    return "{}{}".format(a_netloc, a_path)
+
+
+def save_data_to_dir(output_dir, tag_lists):
+    for _dict in tag_lists:
+        if _dict['type'] == 'img':
+            resp = requests.get(_dict['link'])
+            path_to_file = Path(output_dir / _dict['filename'])
+            path_to_file.write_bytes(resp.content)
+        else:
+            resp = requests.get(_dict['link'])
+            path_to_file = Path(output_dir / _dict['filename'])
+            path_to_file.write_text(resp.text)
+
+
+def format_page(url, output_dir):
+    u = urlparse(url)
+    u_netlog = u.netloc
+
+    name_html = conv_name(url)
     path_to_html = Path(output_dir / name_html)
 
-    name_dir = re.sub(pattern, "-", name_url) + '_files'
+    name_dir = re.sub(r'(.html)$', '_files', name_html)
     output_dir = Path(output_dir / name_dir)
     output_dir.mkdir()
 
-    o = urlparse(url)
-
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, 'html.parser')
-    for img in soup.find_all('img'):
-        i = urlparse(img['src'])
-        if all([i.scheme != '', i.netloc != o.netloc]):
-            continue
-        else:
-            img_url = urljoin(url, img['src'])
-            # https://ru.hexlet.io/assets/java.png
 
-            pattern = r'[\.\:\/]'
-            img_name = re.sub(pattern, "-", o.netloc)
-            # ru-hexlet-io
+    tag_list_all = soup(['link', 'img', 'script'])
 
-            img_name = f"{img_name}{img['src']}"
-            # ru-hexlet-io/assets/java.png
+    tag_lists = []
+    for tag in tag_list_all:
+        if check_tag(tag, u_netlog):
+            tag_dict = {}
+            # замена атрибута тега, список словарей с ресурсами
+            if tag.name == 'link':
+                full_url = urljoin(url, tag['href'])
+                tag['href'] = full_url
+                tag['href'] = "{}/{}".format(name_dir, conv_name(full_url))
 
-            img_name = img_name.replace("/", '-')
-            # ru-hexlet-io-assets-java.png
+                # словарь для загрузки файлов
+                tag_dict['link'] = full_url
+                tag_dict['filename'] = str(conv_name(full_url))
+                tag_dict['type'] = 'text'
+                # список словарей
+                tag_lists.append(tag_dict)
+            else:
+                full_url = urljoin(url, tag['src'])
+                tag['src'] = full_url
+                tag['src'] = "{}/{}".format(name_dir, conv_name(full_url))
 
-            img_path = Path(output_dir / img_name)
-            # .../ru-hexlet-io-assets-java.png
-
-            with open(img_path, 'wb') as f:
-                f.write(requests.get(img_url).content)
-
-            img['src'] = f"{name_dir}/{img_name}"
+                # словарь для загрузки файлов
+                tag_dict['link'] = full_url
+                tag_dict['filename'] = str(conv_name(full_url))
+                tag_dict['type'] = 'img'
+                # список словарей
+                tag_lists.append(tag_dict)
 
     path_to_html.write_text(soup.prettify())
 
-    # p = Path(Path.cwd() / 'tests/fixtures/test2/ru-hexlet-io-courses.html')
-    # soup = BeautifulSoup(p.read_text(), 'html.parser')
-    # print(soup.prettify())
+    # скачивание файлов в папку
+    save_data_to_dir(output_dir, tag_lists)
 
     return path_to_html
 
 
 def download(url, output_dir=None):
     output_dir = Path(Path.cwd() / output_dir)
-    path_to_file = save_data(url, output_dir)
+    path_to_file = format_page(url, output_dir)
 
     return path_to_file
--- tests/test_download.py
+++ tests/test_download.py
@@ -15,37 +15,25 @@ def test_download_returns_path_file_and_folder_name(requests_mock):
         assert path_to_folder.exists()
 
 
-# def test_download_returns_page_right_and_img(requests_mock):
-#     page = Path("tests/fixtures/test1/index.html").read_text()
-#     page_right = Path("tests/fixtures/test1/ru-hexlet-io-courses.html")
-#
-#     img = Path("tests/fixtures/test1/nodejs.png").read_bytes()
-#
-#     requests_mock.get('https://ru.hexlet.io/courses', text=page)
-#     requests_mock.get('https://ru.hexlet.io/assets/professions/nodejs.png', content=img)
-#
-#     with tempfile.TemporaryDirectory() as tmp:
-#         result = download('https://ru.hexlet.io/courses', tmp)
-#
-#         path_to_img = Path(Path(tmp) / 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png')
-#
-#         assert result.read_text() == page_right.read_text()
-#         assert img == path_to_img.read_bytes()
-
-
 def test_download_returns_page_right_and_img(requests_mock):
-    page = Path("tests/fixtures/test2/index.html").read_text()
-    page_right = Path("tests/fixtures/test2/ru-hexlet-io-courses.html")
+    page = Path("tests/fixtures/test/index.html").read_text()
+    page_right = Path("tests/fixtures/test/ru-hexlet-io-courses.html")
 
-    img = Path("tests/fixtures/test1/nodejs.png").read_bytes()
+    img = Path("tests/fixtures/test/nodejs.png").read_bytes()
+
+    script = Path("tests/fixtures/test/index.html").read_text()
 
     requests_mock.get('https://ru.hexlet.io/courses', text=page)
+    requests_mock.get('https://ru.hexlet.io/assets/application.css', text=page)
     requests_mock.get('https://ru.hexlet.io/assets/professions/nodejs.png', content=img)
+    requests_mock.get('https://ru.hexlet.io/packs/js/runtime.js', text=script)
 
     with tempfile.TemporaryDirectory() as tmp:
         result = download('https://ru.hexlet.io/courses', tmp)
 
         path_to_img = Path(Path(tmp) / 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png')
+        path_to_script = Path(Path(tmp) / 'ru-hexlet-io-courses_files/ru-hexlet-io-packs-js-runtime.js')
 
         assert page_right.read_text() == result.read_text()
         assert img == path_to_img.read_bytes()
+        assert script == path_to_script.read_text()
